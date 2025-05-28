0b9b5749df73f45283d953e689fa04458a6b8483
piecrust72 <58156012+piecrust72@users.noreply.github.com>
2025-05-28 09:56:42 -0400

    Add files via upload

---
+++ scripts/universal/main.js
@@ -0,0 +1,8 @@
+(async () => {
+    const cache = await GM_getValue('cache_Universal');
+    alert(`Hello World!\nVersion: ${cache.version}\n${cache.wasFresh ? 'Fresh download!' : 'From cache.'}`);
+    if (cache.wasFresh) {
+        cache.wasFresh = false;
+        await GM_setValue('cache_Universal', cache);
+    }
+})();

234af22b806be4221f4b11f9c7ffd48688af26c7
piecrust72 <58156012+piecrust72@users.noreply.github.com>
2025-05-28 10:28:53 -0500

    Delete scripts directory

--- scripts/testing123/main.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-    const cache = await GM_getValue('cache_Universal');
-    alert(`Hello World!\nVersion: ${cache.version}\n${cache.wasFresh ? 'Fresh download!' : 'From cache.'}`);
-    if (cache.wasFresh) {
-        cache.wasFresh = false;
-        await GM_setValue('cache_Universal', cache);
-    }
-})();

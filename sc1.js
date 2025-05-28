c707d511610f6484ec57c0a4761be9ee76f31885
piecrust72 <58156012+piecrust72@users.noreply.github.com>
2025-05-28 10:27:45 -0500

    Update sc1.js

--- sc1.js
+++ sc1.js
@@ -1,1 +1,13 @@
+// ==UserScript==
+// @name         Hello World Popup
+// @namespace    https://yourdomain.example/
+// @version      1.0
+// @description  Simple Hello World alert to test userscript loading
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
 
+(function () {
+    alert('Hello World');
+})();
